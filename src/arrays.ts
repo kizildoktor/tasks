76092130e6cae093143d23c2ad3e4d8cdcfec9a5
Umut Kurt <kizildoktor@gmail.com>
2022-09-21 12:24:37 -0400

    Completed tripleNumbers

--- src/arrays.ts
+++ src/arrays.ts
@@ -17,7 +17,7 @@ export function bookEndList(numbers: number[]): number[] {
  * number has been tripled (multiplied by 3).
  */
 export function tripleNumbers(numbers: number[]): number[] {
-    return numbers;
+    return numbers.map((item: number): number => item * 3);
 }
 
 /**
