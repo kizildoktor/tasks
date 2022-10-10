7fe03e64236824c2166606bef87785543e229c26
Umut Kurt <kizildoktor@gmail.com>
2022-10-10 14:51:51 -0400

    Completed RevealAnswer.tsx

--- src/components/RevealAnswer.tsx
+++ src/components/RevealAnswer.tsx
@@ -2,5 +2,15 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function RevealAnswer(): JSX.Element {
-    return <div>Reveal Answer</div>;
+    const [visible, switchVisibility] = useState<boolean>(false);
+    const visibility = (): void => {
+        switchVisibility(!visible);
+    };
+    return (
+        <div>
+            <Button onClick={visibility}>
+                Reveal Answer {visible && <div>42</div>}
+            </Button>
+        </div>
+    );
 }
