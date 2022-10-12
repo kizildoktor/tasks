9d032138ad1ce28aa4c9b9e62e717aad714b5aa2
Umut Kurt <kizildoktor@gmail.com>
2022-10-12 15:25:58 -0400

    Modified App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import "./App.css";
 import { ChangeType } from "./components/ChangeType";
 import { RevealAnswer } from "./components/RevealAnswer";
@@ -13,7 +14,43 @@ function App(): JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
-
+            <h1>Umut Kurt</h1>
+            <img
+                src="https://i.imgur.com/WGovLth.png"
+                width="200"
+                height="200"
+                alt="I like this image."
+            />
+            <ol>
+                <li>Item #1.</li>
+                <li>Item #2.</li>
+                <li>Item #3.</li>
+            </ol>
+            <Container>
+                <Row>
+                    <Col>
+                        <div
+                            style={{
+                                width: "100px",
+                                height: "100px",
+                                backgroundColor: "red"
+                            }}
+                        ></div>
+                    </Col>
+                    <Col>
+                        <div
+                            style={{
+                                width: "100px",
+                                height: "100px",
+                                backgroundColor: "red"
+                            }}
+                        ></div>
+                    </Col>
+                </Row>
+            </Container>
+            <Button onClick={() => console.log("Hello World!")}>
+                Log Hello World
+            </Button>
             <hr></hr>
             <Counter></Counter>
             <hr />
