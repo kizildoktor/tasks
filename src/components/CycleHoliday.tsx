dcfa9c855943e40a7a7ed68600cea0ec7dbb36de
Umut Kurt <kizildoktor@gmail.com>
2022-10-12 15:24:03 -0400

    Completed CycleHoliday.tsx

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -1,6 +1,44 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
+type Holiday =
+    | "Holiday: 🎃"
+    | "Holiday: 🧨"
+    | "Holiday: 🐉"
+    | "Holiday: 🦃"
+    | "Holiday: 🐰";
+
+const alphabeticOrder: Record<Holiday, Holiday> = {
+    "Holiday: 🐉": "Holiday: 🐰",
+    "Holiday: 🐰": "Holiday: 🎃",
+    "Holiday: 🎃": "Holiday: 🧨",
+    "Holiday: 🧨": "Holiday: 🦃",
+    "Holiday: 🦃": "Holiday: 🐉"
+};
+
+const chronologicOrder: Record<Holiday, Holiday> = {
+    "Holiday: 🐉": "Holiday: 🧨",
+    "Holiday: 🧨": "Holiday: 🐰",
+    "Holiday: 🐰": "Holiday: 🎃",
+    "Holiday: 🎃": "Holiday: 🦃",
+    "Holiday: 🦃": "Holiday: 🐉"
+};
+
 export function CycleHoliday(): JSX.Element {
-    return <div>Cycle Holiday</div>;
+    const [holiday, setHoliday] = useState<Holiday>("Holiday: 🐉");
+    function alphabeticCycle(): void {
+        const nextHoliday: Holiday = alphabeticOrder[holiday];
+        setHoliday(nextHoliday);
+    }
+    function chronologicCycle(): void {
+        const nextHoliday: Holiday = chronologicOrder[holiday];
+        setHoliday(nextHoliday);
+    }
+    return (
+        <div>
+            <Button onClick={alphabeticCycle}>Advance by Alphabet</Button>{" "}
+            <Button onClick={chronologicCycle}>Advance by Year</Button>{" "}
+            <Button>{holiday}</Button>
+        </div>
+    );
 }
