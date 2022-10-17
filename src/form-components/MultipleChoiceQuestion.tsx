5b064b0290b01dc0b8c4ffa63ad3ee48965dda2f
Umut Kurt <kizildoktor@gmail.com>
2022-10-16 22:17:47 -0400

    Completed MultipleChoice.tsx

--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function MultipleChoiceQuestion({
     options,
@@ -7,9 +8,21 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): JSX.Element {
+    const [choice, setChoice] = useState<string>(options[0]);
+    function updateChoice(event: React.ChangeEvent<HTMLSelectElement>) {
+        setChoice(event.target.value);
+    }
     return (
         <div>
-            <h3>Multiple Choice Question</h3>
+            <h3 className="title">Multiple Choice Question</h3>
+            <Form.Group>
+                <Form.Select onChange={updateChoice}>
+                    {options.map((option: string) => (
+                        <option key={option} value={option} label={option} />
+                    ))}
+                </Form.Select>
+            </Form.Group>
+            {choice === expectedAnswer ? "✔️" : "❌"}
         </div>
     );
 }
